fc0dfce4286a24492014f9c3ac3c34fbc95063f3
Joshua Moon <joshua@moon.uk.net>
2018-11-11 14:44:07 +0000

    Pulls data from OpenWeatherMap for current day. Uses injected services

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  appId: '08680658461ed526358f5dea8fd4f4f3',
+  baseUrl: 'http://',
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
